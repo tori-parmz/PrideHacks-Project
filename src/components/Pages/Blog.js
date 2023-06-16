39648d8859765e7f8de7b7f86a3004e71bf7e3c9
akebu6 <asimasiku999@gmail.com>
2023-06-17 01:01:19 +0200

    chore: updated blogs

--- src/components/Pages/Blog.js
+++ src/components/Pages/Blog.js
@@ -1,71 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
 
 function Blog() {
   return (
-    <Container>
-      <Row>
-        <Col xs={6}>
-          <Card>
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={6}>
-          <Card>
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-    </Row>
-    <br />
-    <Row>
-        <Col xs={6}>
-          <Card>
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={6}>
-          <Card>
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-    </Row>
-    </Container>
-    
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>This is the Blog</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   );
 }
 
--- src/components/Pages/UserBlog.js
+++ src/components/Pages/UserBlog.js
@@ -1,18 +1,92 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+
+// importing the blog images
+import prideCelebration from '../../blog_images/pride_celebration.jpg';
+import prideFlags from '../../blog_images/pride_flag.jpg';
+import prideParade from '../../blog_images/pride_parade.jpg';
+import prideLove from '../../blog_images/pride_love.jpg';
 
 function UserBlog() {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>This is the User Blog</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <Container>
+      <Row>
+        <Col xs={6}>
+          <Card>
+            <Card.Img variant="top" src={prideParade} height={280} />
+            <Card.Body>
+              <Card.Title> My First Pride Parade</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <Button variant="primary">Read more</Button>
+            </Card.Body>
+            <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+          </Card>
+        </Col>
+        <Col xs={6}>
+          <Card>
+            <Card.Img variant="top" src={prideCelebration} height={280} />
+            <Card.Body>
+              <Card.Title>My Pride Celebration</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <Button variant="primary">Read more</Button>
+            </Card.Body>
+            <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+          </Card>
+        </Col>
+    </Row>
+    <br />
+    <Row>
+        <Col xs={6}>
+          <Card>
+            <Card.Img variant="top" src={prideFlags} height={280} />
+            <Card.Body>
+              <Card.Title>Different Pride Flags</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <Button variant="primary">Read more</Button>
+            </Card.Body>
+            <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+          </Card>
+        </Col>
+        <Col xs={6}>
+          <Card>
+            <Card.Img variant="top" src={prideLove} height={280} />
+            <Card.Body>
+              <Card.Title>My First Love</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <Button variant="primary">Read more</Button>
+            </Card.Body>
+            <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+          </Card>
+        </Col>
+    </Row>
+    </Container>
   );
 }
 
